f2f8e715062cc4323c417d526ef4534246bdf3f7
Max McDonough <max.mcdonough@gmail.com>
2022-10-03 12:43:11 -0400

    Working map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,23 +1,27 @@
-// import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 // import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"; 
 
 
-const Map = () => {
-    return (
-        <div>
-            <p>Map test</p>
-        </div>
-    )
-};
+// const ref = React.useRef(null);
+// const [map, setMap] = React.useState();
 
-
-
-
-
+// React.useEffect(() => {
+//   if (ref.current && !map) {
+//     setMap(new window.google.maps.Map(ref.current, {}));
+//   }
+// }, [ref, map]);
 
 
 
-export default Map;
+// const Map = () => {
+//     return (
+//         <GoogleMap zoom={10} center={{lat: 30, lng: -90}} mapContainerClassName="w-100 h-100">
+//         </GoogleMap>
+//     )
+// };
+
+// export default Map;
 
 
 
--- client/src/components/MapWrapper.js
+++ client/src/components/MapWrapper.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react';
-import { Wrapper, Status, Spinner, ErrorCompnent } from "@googlemaps/react-wrapper";
+// import { Wrapper, Status, Spinner, ErrorCompnent } from "@googlemaps/react-wrapper";
+import { GoogleMap, LoadScript, useLoadScript, Marker } from "@react-google-maps/api"; 
 
-import Map from './Map';
+// import Map from './Map';
 
-const render = (status) => {
-    switch (status) {
-        case Status.LOADING:
-            return <Spinner />;
-        case Status.FAILURE:
-            return <ErrorCompnent />;
-        default:
-            return <Map />;
-    }
-};
+// const render = (status) => {
+//     switch (status) {
+//         case Status.LOADING:
+//             return <Spinner />;
+//         case Status.FAILURE:
+//             return <ErrorCompnent />;
+//         default:
+//             return <Map />;
+//     }
+// };
+
+const apiKey = 'mykey';
+
+const containerStyle = {
+    width: '90vw',
+    height: '90vh'
+  };
 
 const MapWrapper = () => {
-    return (
-        <Wrapper apiKey={"YOUR_API_KEY"} render={render} />
-    )
+    // const isLoaded = useLoadScript({
+        
+    // });
+        return (
+        <LoadScript googleMapsApiKey={apiKey}>
+            <GoogleMap 
+                zoom={10} 
+                center={{lat: 30, lng: -90}}
+                mapContainerStyle={containerStyle}
+                >
+            </GoogleMap>
+        </LoadScript>
+        )
 };
+
 
 export default MapWrapper;
 
