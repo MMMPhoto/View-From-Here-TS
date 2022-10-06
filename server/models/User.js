a0fdc2f1ed73a3584ab424eb91d441c1d83ba312
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-10-06 16:37:05 -0400

    Created User.js, with password hash and a virtual for getting their number of submitted pictures

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import picSchema from "./Picture";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    savedPics: [picSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.virtual("picCount").get(function () {
+  return this.savedPics.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
