05fb629a5addf485eb82ecf1dbcb37c08009de03
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-10-08 15:08:32 -0400

    Added routes for login, savepic, and deletesavedpic, as well as importing auth middleware

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,3 +1,4 @@
+import { authMiddleware } from "../../utils/auth.js";
 import express from "express";
 const router = express.Router();
 
@@ -7,10 +8,21 @@ import {
   createNewUser,
   updateUser,
   deleteUser,
+  login,
+  savePic,
+  deleteSavedPic,
 } from "../../controllers/users-controller.js";
 
-router.route("/").get(getAllUsers).post(createNewUser);
+router
+  .route("/")
+  .get(getAllUsers)
+  .post(createNewUser)
+  .put(authMiddleware, savePic);
+
+router.route("/login").post(login);
 
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/pics/:picId").delete(authMiddleware, deleteSavedPic);
 
 export default router;
