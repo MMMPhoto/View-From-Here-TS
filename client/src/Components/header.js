3fe169dd7b44042bb59fa14fb7d8c60b4d081b45
Max McDonough <max.mcdonough@gmail.com>
2022-10-10 21:04:03 -0400

    Fixed Home link always active

--- client/src/Components/header.js
+++ client/src/Components/header.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-            <a className="navbar-brand" href="#!">View From Here</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <Link className="nav-link active" to="/"><li className="nav-item">Home</li></Link>
-                    <Link className="nav-link" to="/login"><li className="nav-item">Login</li></Link>
-                    <Link className="nav-link" to="signup"><li className="nav-item">Signup</li></Link>
-                </ul>
+            <div className="container">
+                <Link className="navbar-brand" to="/">View From Here</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <NavLink className="nav-link" to="/" end><li className="nav-item">Home</li></NavLink>
+                        <NavLink className="nav-link" to="/login"><li className="nav-item">Login</li></NavLink>
+                        <NavLink className="nav-link" to="signup"><li className="nav-item">Signup</li></NavLink>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-    
+        </nav>
     )
-
 };
 
 export default Header;
