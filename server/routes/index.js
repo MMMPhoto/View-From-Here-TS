15c9a43a1a2d74071c6ad185f75f8dbc79889660
Max McDonough <max.mcdonough@gmail.com>
2022-10-11 20:43:46 -0400

    last deploy test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,16 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = fileURLToPath(import.meta.url);
-
 import apiRoutes from "./api/index.js";
 const router = express.Router();
 
 router.use("/api", apiRoutes);
 
-// serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-});
-
 // router.use((req, res) => {
 //   return res.send("Your route sucks and you should feel bad about yourself.");
+// });
+
+// // serve up react front-end in production
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 // });
 
 export default router;
