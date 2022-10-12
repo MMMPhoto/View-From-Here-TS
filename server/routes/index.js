f542bd71134bc93909dfe7ffaba9d10310b97cef
Max McDonough <max.mcdonough@gmail.com>
2022-10-11 21:20:54 -0400

    Added back in route error

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 router.use("/api", apiRoutes);
 
-// router.use((req, res) => {
-//   return res.send("Your route sucks and you should feel bad about yourself.");
-// });
+router.use((req, res) => {
+  return res.send("Your route sucks and you should feel bad about yourself.");
+});
 
 // // serve up react front-end in production
 // router.use((req, res) => {
