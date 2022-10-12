0a017922d97ab09c61650e61610a68ca843e4b3e
Max McDonough <max.mcdonough@gmail.com>
2022-10-12 12:14:50 -0400

    Saved photo buttons( no function yet

--- client/src/pages/SingleView.js
+++ client/src/pages/SingleView.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getOnePic } from '../utils/api';
+import { GrFavorite } from 'react-icons/gr';
 import MapWrapper from '../components/MapWrapper';
 import './SingleView.css';
 
@@ -8,6 +9,9 @@ const SingleView = () => {
     const { pictureId } = useParams();
     const [pictureData, setPictureData] = useState([{}]);
     const [picUrl, setPicUrl] = useState("");
+
+    // Set state for saved photo
+    const [isSavedPhoto, setSavedPhoto] = useState(false);
 
     // Map Container Styling
     const containerStyle = {
@@ -32,9 +36,19 @@ const SingleView = () => {
         };
         getPicData();
     }, []);
+
+      // Handle save photo
+      const handleSavePhoto = () => {
+        // To Do: Add work to save to user's saved photos in database
+        setSavedPhoto(true);
+    };
     
     return (
         <div className='d-flex flex-column align-items-center p-4'>
+            <div className='d-flex flex-row justify-content-center align-items-center'>
+                <h3><GrFavorite onClick={(() => handleSavePhoto())}/></h3>
+                { isSavedPhoto ? (<p className="ms-3 mb-0">Photo saved!</p>) : (<></>)}
+            </div>
             <img className='single-pic p-4' src={picUrl} />
             <div>
                 { pictureData[0].lat ? (<MapWrapper markers={pictureData} containerStyle={containerStyle} />) : (
