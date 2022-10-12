7c0a5f99b47f6c00ace46e5709b69a289c0ae5b8
Max McDonough <max.mcdonough@gmail.com>
2022-10-12 11:12:36 -0400

    Added favorite icon and confirmation to info window

--- client/src/components/MarkerInfoCard.js
+++ client/src/components/MarkerInfoCard.js
@@ -1,18 +1,32 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { GoogleMap, LoadScript, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import { GrFavorite } from 'react-icons/gr';
 import './MarkerInfoCard.css';
 
 const MarkerInfoCard = ({marker, navigate}) => {
+
+    // Set state for saved photo
+    const [isSavedPhoto, setSavedPhoto] = useState(false);
 
     // Set url to lower resolution for thumbnail
     let urlArray  = marker.url.split("/");
     let url = `${urlArray[0]}/${urlArray[1]}/${urlArray[2]}/${urlArray[3]}/${urlArray[4]}/${urlArray[5]}/c_scale,w_150/${urlArray[6]}/${urlArray[7]}`;
     url = url.replace('heic', 'jpg');
 
+    // Handle save photo
+    const handleSavePhoto = () => {
+        // To Do: Add work to save to user's saved photos in database
+        setSavedPhoto(true);
+    };
+
     return (
-        <div className='info-div' onClick={() => navigate(`/single-view/${marker.id}`)}>
+        <div className='info-div'>
             <h6>{marker.title}</h6>
-            <img className="marker-thumb" src={url} />
+            <img className="marker-thumb" src={url} onClick={() => navigate(`/single-view/${marker.id}`)}/>
+            <div className='d-flex flex-row justify-content-around align-items-center mt-2'>
+                <h3><GrFavorite onClick={(() => handleSavePhoto())}/></h3>
+                { isSavedPhoto ? (<p className="mb-0">Photo saved!</p>) : (<></>)}
+            </div>
         </div>
        
     )
