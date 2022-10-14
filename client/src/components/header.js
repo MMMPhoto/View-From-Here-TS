192d22977141c8d14b0d79089b6ecd9f3e0f8712
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-10-14 15:10:44 -0400

    overwrote default bootstrap colors for header links

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,7 +8,7 @@ import './header.css'
 function Header(props) {
     
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark"id="header" >
+        <nav className="navbar navbar-expand-lg navbar-dark "id="header" >
             <div className="container" >
             <Link className="navbar-brand" id="title" to="/">View From Here</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span><i class="fa-solid fa-cloud"></i></button>
