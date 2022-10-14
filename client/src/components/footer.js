2f9756adb9cd222e28f3094c711afa7075ab729a
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-10-14 10:51:13 -0400

    added mountain image to footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'; 
+import './footer.css'
 
 class Footer extends Component {
 
     render() {
 
         return (
-            <footer className="py-5 bg-dark">
-            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+            <footer className="py-5 bg-dark" id ="footer">
+            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; View-From-Here 2022</p></div>
         </footer>
 
 
