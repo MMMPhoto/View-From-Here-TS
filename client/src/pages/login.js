1b9d77346f0610d66e44efc71cbb51ad40ead442
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-10-17 15:40:56 -0400

    added images to login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 // import Header from '../components/header';
 // import Footer from '../components/footer';
 import Auth from "../utils/auth";
+import './login.css'
 
 function Login(props) {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
@@ -54,7 +55,7 @@ function Login(props) {
   };
   return (
     <>
-      <section className="vh-100">
+      <section className="vh-100" id = 'background'>
         <div className="container h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-lg-12 col-xl-11">
@@ -120,7 +121,7 @@ function Login(props) {
                     </div>
                     <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                       <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                        src="https://res.cloudinary.com/dwuqez3pg/image/upload/c_scale,w_500/v1665696442/View-from-here/1ddfeb86305588512f79432b4a107ec5.jpg"
                         className="img-fluid"
                         alt="Sample image"
                       />
