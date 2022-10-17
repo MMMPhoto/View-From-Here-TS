92db8e40b342493022ca0eea4df3fe38325183e3
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-10-17 15:50:08 -0400

    added opacity to login form:

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -57,17 +57,17 @@ function Login(props) {
     <>
       <section className="vh-100" id = 'background'>
         <div className="container h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-lg-12 col-xl-11">
+          <div className="row d-flex justify-content-center align-items-center h-100"id = 'formbg' >
+            <div className="col-lg-12 col-xl-11" >
               <div className="card text-black">
-                <div className="card-body p-md-5">
+                <div className="card-body p-md-5" >
                   <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                        Login
+                        Welcome Back!
                       </p>
 
-                      <form className="mx-1 mx-md-4">
+                      <form className="mx-1 mx-md-4" >
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                           <div className="form-outline flex-fill mb-0">
