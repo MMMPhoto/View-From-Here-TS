cffe28aaf664b61cf8389df70b7672b9c56f3e71
Max McDonough <max.mcdonough@gmail.com>
2022-11-02 21:39:53 -0400

    Added saved photos reducer file to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mapStateReducer from '../features/mapState/mapStateSlice';
+import userSavedPhotosReducer from '../features/userSavedPhotos/userSavedPhotosSlice';
 
 export default configureStore({
-  reducer: { mapState: mapStateReducer },
+  reducer: { mapState: mapStateReducer, userSavedPhotos: userSavedPhotosReducer },
 });
