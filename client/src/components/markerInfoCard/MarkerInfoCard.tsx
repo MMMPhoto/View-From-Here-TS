4dbdb3e804ec99e5e801a285e354075d3c480ca0
Max McDonough <max.mcdonough@gmail.com>
2023-02-06 22:17:07 -0500

    Marker info card to tsx

--- client/src/components/markerInfoCard/MarkerInfoCard.tsx
+++ client/src/components/markerInfoCard/MarkerInfoCard.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import { FC } from "react";
 import "./MarkerInfoCard.css";
 import { useSpring, animated, easings } from 'react-spring';
+import { Photo } from "../../types/Photo";
 
-const MarkerInfoCard = ({ marker, navigate }) => {
+const MarkerInfoCard: FC<{ marker: Photo, navigate: Function }> = ({ marker, navigate }) => {
   // Call API for photo URL
-  const url = `https://res.cloudinary.com/dwuqez3pg/image/upload/c_scale,w_150/v1665696442/${marker.public_id}.jpg`;
-  const picStyles = useSpring({
+  const url: string = `https://res.cloudinary.com/dwuqez3pg/image/upload/c_scale,w_150/v1665696442/${marker.public_id}.jpg`;
+  const picStyles: any = useSpring({
     from: { opacity: 0, transform: 'scale(0)', maxHeight: '0px', maxWidth: '0px' },
     to: { opacity: 1, transform: 'scale(1)', maxHeight: '1000px', maxWidth: '1000px' },
     // leave: { opacity: 0, transform: 'scaleY(0)', maxHeight: '0px' },
