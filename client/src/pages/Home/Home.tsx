32046c4f370cb268740b78b99cdb85bfe07e80c8
Max McDonough <max.mcdonough@gmail.com>
2023-04-03 17:18:13 -0400

    Created styles file for Home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React, { FC, useEffect, useState, useRef } from "react";
 import { getAllPics } from "../../utils/api";
 import MapWrapper from "../../components/Map/MapWrapper";
-import { useSpring, animated, easings } from 'react-spring';
 import { useSelector, useDispatch } from 'react-redux';
 import { saveMarkers, saveBounds, selectMarkers, selectBounds } from "../../store/mapStateSlice";
 import { Photo } from "../../types/Photo";
+import { MapDiv } from "./styles";
 
 const Home: FC<{}> = () => {
 
@@ -14,17 +14,6 @@ const Home: FC<{}> = () => {
   // Define React Redux functions
   const savedMarkers = useSelector(selectMarkers);
   const dispatch = useDispatch();
-
-  // Query height of window to set map height, accounting for header height
-  // const heightRef = useRef([window.innerHeight]);
-  // const mapHeight = `${heightRef.current[0]}px`;
-
-  const divStyles: any = useSpring({
-    from: { opacity: 1, transform: 'scale(0)', maxHeight: '0px', maxWidth: '0px' },
-    to: { opacity: 1, transform: 'scale(1)', maxHeight: '100%', maxWidth: '100%', flexGrow: "1", borderWidth: "10px", display: "flex", justifyContent: "center" },
-    // leave: { opacity: 0, transform: 'scaleY(0)', maxHeight: '0px' },
-    config: { duration: 500, easing: easings.easeInOutQuad }
-  });
 
   // Map Container Styling
   const containerStyle = {
@@ -48,18 +37,12 @@ const Home: FC<{}> = () => {
   }, []);
 
   return (
-    // <animated.div
-    <div
-      // style={divStyles}
-      style={{ flexGrow: "1", borderWidth: "10px", display: "flex", justifyContent: "center" }}
-    >
+    <MapDiv>
       <MapWrapper
         markers={markers}
         containerStyle={containerStyle}
       />
-    {/* </animated.div> */}
-    </div>
-
+    </MapDiv>
   );
 };
 
---
+++ client/src/pages/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MapDiv = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+`;
