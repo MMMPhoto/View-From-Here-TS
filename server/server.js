6370830555107edb5cbb5c7511096eb4be3eb959
Max McDonough <max.mcdonough@gmail.com>
2023-04-20 10:27:16 -0400

    Test secure connection redirect for heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-}
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  });
+};
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
