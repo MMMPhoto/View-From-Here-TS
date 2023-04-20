fa4f260773adf6c287acaa95527e7227eeb41102
Max McDonough <max.mcdonough@gmail.com>
2023-04-20 13:17:58 -0400

    Try again on SSL thing

--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ app.use(express.json());
 
 // Production Build Check Middleware
 if (process.env.NODE_ENV === "production") {
-  // app.use((req, res, next) => {
-  //   if (req.header('x-forwarded-proto') !== 'https')
-  //     res.redirect(`https://${req.header('host')}${req.url}`)
-  //   else
-  //     next()
-  // });
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  });
   app.use(express.static(path.join(__dirname, "../client/build")));
 };
 
