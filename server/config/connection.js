92e7fa12a28e64619ecd7ab0cd9c70632ff72c12
Max McDonough <max.mcdonough@gmail.com>
2023-05-05 10:59:45 -0400

    Added docker files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/view-from-here",
+  process.env.MONGODB_URI || "mongodb://db:27017/view-from-here",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
